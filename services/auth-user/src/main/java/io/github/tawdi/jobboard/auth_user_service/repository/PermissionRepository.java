f52cd5fc71d6a54dc6059d58bd1f702f73948df3
Tawdi <ahmedtawdi00@gmail.com>
2026-02-02 17:00:05 +0100

    add repositories for auth service

---
+++ services/auth-user/src/main/java/io/github/tawdi/jobboard/auth_user_service/repository/PermissionRepository.java
@@ -0,0 +1,13 @@
+package io.github.tawdi.jobboard.auth_user_service.repository;
+
+import io.github.tawdi.jobboard.auth_user_service.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Optional<Permission> findByName(String name);
+    boolean existsByName(String name);
+}
