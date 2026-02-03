8761bfcdb95b7f312ed612fe3eec5ea639321586
Tawdi <ahmedtawdi00@gmail.com>
2026-02-03 11:58:01 +0100

    add services for auth and userDetails

---
+++ services/auth-user/src/main/java/io/github/tawdi/jobboard/auth_user_service/service/UserService.java
@@ -0,0 +1,61 @@
+package io.github.tawdi.jobboard.auth_user_service.service;
+
+import io.github.tawdi.jobboard.auth_user_service.dto.*;
+import io.github.tawdi.jobboard.auth_user_service.entity.User;
+import io.github.tawdi.jobboard.auth_user_service.repository.RoleRepository;
+import io.github.tawdi.jobboard.auth_user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return UserResponse.builder()
+            .id(user.getId())
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .name(user.getName())
+            .role(user.getRole().getName())
+            .build();
+    }
+
+    public UserResponse getUserById(String userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return UserResponse.builder()
+            .id(user.getId())
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .name(user.getName())
+            .role(user.getRole().getName())
+            .build();
+    }
+
+    public UserResponse getUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return UserResponse.builder()
+            .id(user.getId())
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .name(user.getName())
+            .role(user.getRole().getName())
+            .build();
+    }
+
+}
