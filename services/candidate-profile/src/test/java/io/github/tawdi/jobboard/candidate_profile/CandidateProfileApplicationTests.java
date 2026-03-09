2da704fd95be867f549c26cc56b89d0ea3d6570a
Tawdi <ahmedtawdi00@gmail.com>
2026-03-09 10:49:25 +0000

    add dockerfile for condidate profile microservice

---
+++ services/candidate-profile/src/test/java/io/github/tawdi/jobboard/candidate_profile/CandidateProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tawdi.jobboard.candidate_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
