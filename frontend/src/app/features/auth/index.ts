431b442b95c78b4f3d609ef3be7b3f2144aae618
Tawdi <ahmedtawdi00@gmail.com>
2026-03-24 09:00:21 +0100

    add missing pages for auth

--- frontend/src/app/features/auth/index.ts
+++ frontend/src/app/features/auth/index.ts
@@ -1,3 +1,6 @@
 export * from './login/login';
 export * from './register/register';
-
+export * from './verify-email/verify-email';
+export * from './resend-verification/resend-verification';
+export * from './forgot-password/forgot-password';
+export * from './reset-password/reset-password';
